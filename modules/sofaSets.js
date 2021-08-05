33d1d8a4bf41ebdd27fed80a3c715b09a3de4ac4
harman <harmanave@gmail.com>
2021-08-06 02:03:37 +0530

    added sofa images & updated links

--- modules/sofaSets.js
+++ modules/sofaSets.js
@@ -1,36 +1,42 @@
 // sofaSets.js
 
-// export default function sofaImages() {
+export default function sofaImages() {
 
-//     const imagesArray = [{
-//             path: './assets/sofa-1.JPG',
-//             alt: 'Sofa Set'
-//             },
-//         {
-//             path: './assets/sofa-2.JPG',
-//             alt: 'Sofa Set'
-//             },
-//         {
-//             path: './assets/sofa-3.JPG',
-//             alt: 'Sofa Set'
-//             },
-//         {
-//             path: './assets/sofa-4.JPG',
-//             alt: 'Sofa Set'
-//         }
-//     ]
+    const imagesArray = [{
+            path: './assets/sofa-1.JPG',
+            alt: 'Sofa Set'
+            },
+        {
+            path: './assets/sofa-2.JPG',
+            alt: 'Sofa Set'
+            },
+        {
+            path: './assets/sofa-3.JPG',
+            alt: 'Sofa Set'
+            },
+        {
+            path: './assets/sofa-4.JPG',
+            alt: 'Sofa Set'
+        }
+    ]
 
-//     imagesArray.forEach((image) => {
+    imagesArray.forEach((image) => {
 
-//         const ssImages = document.querySelector('.ss-images');
+        const ssImages = document.querySelector('.ss-images');
 
-//         const imageDiv = document.createElement('div');
+        const imageDiv = document.createElement('div');
 
-//         imageDiv.style.backgroundImage = `url(${image.path})`;
+        const sofaImage = document.createElement('img');
 
-//         ssImages.appendChild(imageDiv);
-//     })
+        sofaImage.src = `${image.path}`;
 
-// }
+        imageDiv.appendChild(sofaImage);
 
-// sofaImages();
+        // imageDiv.style.backgroundImage = `url(${image.path})`;
+
+        ssImages.appendChild(imageDiv);
+    })
+
+}
+
+sofaImages();
