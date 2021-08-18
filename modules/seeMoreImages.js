5cbfd9c2c7e0d0db005e76ebc8d8ce34b62879dd
harman <harmanave@gmail.com>
2021-08-18 12:41:45 +0530

    see-more styles

--- modules/seeMoreImages.js
+++ modules/seeMoreImages.js
@@ -1,8 +1,11 @@
 // seeMoreImages.js
 
 export default function seeMoreImages(moreImagesArray) {
+
     const seeMoreButton = document.querySelector('.see-more-button');
     const moreProductImages = document.querySelector('.more-product-images');
+
+    seeMoreButton.style.display = 'inline-block';
 
     const productArrayLength = document.createElement('p');
     productArrayLength.textContent = `(${moreImagesArray.length})`;
